2d7bd99c36e2b308b4c7dfbb7055e7115fba0c56
league <league@imac-14.attlocal.net>
2022-10-12 16:56:51 -0700

    barely one

--- src/_99_extra/pin_the_tail/PinTheTail.java
+++ src/_99_extra/pin_the_tail/PinTheTail.java
@@ -14,28 +14,34 @@ public class PinTheTail extends PApplet {
     static final int HEIGHT = 600;
     PImage donkey;
     PImage tail;
+    PImage cat;
     
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
+        
     }
 
     @Override
     public void setup() {
     	donkey = loadImage("donkey.jpg");
     	tail = loadImage("tail.png");
+    	cat = loadImage("cat.jpg");
     	size(WIDTH, HEIGHT);
-    	tail.resize(80, 60);  
-    }
+    	tail.resize(80, 60); 
+    	donkey.resize(WIDTH,HEIGHT);
+    	cat.resize(WIDTH, HEIGHT);
+    	rect(694, 201, 80, 80);
+}
 
     @Override
     public void draw() {
-    	donkey.resize(WIDTH,HEIGHT);
-        background(donkey);
-        image(tail, mouseX, mouseY);
-        rect(0, 0, 30, 30);
-        rect(694, 201, 80, 60);
-        
+        background(cat);
+        image(tail, mouseX - 40, mouseY - 30);
+        if(dist(694, 201, mouseX, mouseY) < 80) {
+    		background(donkey);
+    		playWhoohoo();
+    	}
     }
 
     static public void main(String[] args) {
--- src/_99_extra/pong/Pong.java
+++ src/_99_extra/pong/Pong.java
@@ -5,6 +5,7 @@ import processing.core.PApplet;
 public class Pong extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
+int xSpeed = 80;
 
     @Override
     public void settings() {
@@ -13,12 +14,19 @@ public class Pong extends PApplet {
 
     @Override
     public void setup() {
-
+    	if(xSpeed > WIDTH){
+    	    xSpeed = -xSpeed;
+    	}
     }
 
     @Override
     public void draw() {
-
+    	xSpeed = xSpeed + 1;
+    	int x = 80;
+    	x = xSpeed;
+    	fill(0, 0, 255);
+    	stroke(0, 0, 0);
+    	ellipse(x, 60, 50, 50);
     }
 
     static public void main(String[] args) {
